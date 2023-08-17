c96b2cc9fd5295919f979a7e601c49e0244ddb21
GinoongFlores <florespaul161@gmail.com>
2023-08-17 19:24:30 +0800

    feat(Navbar): apply tw-elements navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,9 @@ import {
 	FaFacebook,
 } from "react-icons/fa";
 
+import { Dropdown, initTE } from "tw-elements";
+initTE({ Dropdown });
+
 const Navbar = () => {
 	const [toggleNav, setToggleNav] = useState(false);
 	const links = [
@@ -71,13 +74,22 @@ const Navbar = () => {
 		return (
 			<li
 				key={index}
-				className={
-					toggleNav
-						? "py-6 text-2xl"
-						: "cursor-pointer px-4 hover:text-blue-400 hover:font-medium"
-				}
+				className="my-4 pl-2 lg:my-0 lg:pl-2 lg:pr-1 xs:pl-0 xs:pr-0 xs:border-b-2 xs:hover:border-blue-600 lg:border-b-0"
+				data-te-nav-item-ref
 			>
-				<a href={link.link}>{link.name}</a>
+				<a
+					onClick={handleToggleNav}
+					href={link.link}
+					className={
+						toggleNav
+							? "active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+							: "p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+					}
+					// aria-current="page"
+					data-te-nav-link-ref
+				>
+					{link.name}
+				</a>
 			</li>
 		);
 	});
@@ -101,41 +113,65 @@ const Navbar = () => {
 	return (
 		<>
 			<header name="home">
-				<div className="sticky w-full h-[80px] flex justify-between items-center px-4">
-					<div>
-						<img
-							src="/img/assets/gdscsticdo-transparent.png"
-							alt="test"
-							width={160}
-						/>
-					</div>
-
-					{/* menu */}
-					<ul className="hidden md:flex">{mapLinks}</ul>
-
-					{/* hamburger */}
-					<div className="md:hidden z-10" onClick={handleToggleNav}>
-						{toggleNav ? <FaTimes /> : <FaBars />}
-					</div>
-
-					{/* mobile menu */}
-					<ul
-						className={
-							toggleNav
-								? "absolute top-0 left-0 w-full h-screen bg-[#414141] flex flex-col justify-center items-center"
-								: "hidden"
-						}
-					>
-						{mapLinks}
-					</ul>
-
-					{/* social icons */}
-					{/* <div className="hidden lg:flex lg:fixed flex-col top-[35%] left-0">
-					<ul>{mapSocials}</ul>
-				</div> */}
-				</div>
-
 				{/* banner */}
+				<nav
+					className="relative flex w-full flex-nowrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:flex-wrap lg:justify-start lg:py-4"
+					data-te-navbar-ref
+				>
+					<div className="flex w-full flex-wrap items-center justify-between px-3">
+						<div className="ml-2">
+							<a
+								className="text-xl text-neutral-800 dark:text-neutral-200"
+								href="#"
+							>
+								<img
+									src="/img/assets/gdscsticdo-transparent.png"
+									alt="gdscsticdo-logo"
+									loading="lazy"
+									width={160}
+								/>
+							</a>
+						</div>
+
+						<button
+							className="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
+							type="button"
+							data-te-collapse-init
+							data-te-target="#navbarSupportedContent2"
+							aria-controls="navbarSupportedContent2"
+							aria-expanded="false"
+							aria-label="Toggle navigation"
+						>
+							<span className="[&>svg]:w-7">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									className="h-7 w-7"
+								>
+									<path
+										fillRule="evenodd"
+										d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+										clipRule="evenodd"
+									/>
+								</svg>
+							</span>
+						</button>
+
+						<div
+							className="!visible mt-2 hidden flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto"
+							id="navbarSupportedContent2"
+							data-te-collapse-item
+						>
+							<ul
+								className="list-style-none ml-auto flex flex-col pl-0 lg:mt-1 lg:flex-row items-center justify-center"
+								data-te-navbar-nav-ref
+							>
+								{mapLinks}
+							</ul>
+						</div>
+					</div>
+				</nav>
 
 				<img src={Banner} alt="banner" className="w-full" />
 			</header>
