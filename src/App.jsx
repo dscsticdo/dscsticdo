e950e7f973cc4b4f4bacc65e385533a31c61d909
GinoongFlores <florespaul161@gmail.com>
2023-08-17 16:45:14 +0800

    initialize app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
 import AboutUs from "./components/AboutUs";
+import Membership from "./components/Membership/Membership";
+import Team from "./components/Team/Team";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<AboutUs />
+			<Membership />
+			<Team />
 		</>
 	);
 }
