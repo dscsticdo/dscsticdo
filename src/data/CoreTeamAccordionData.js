be25df5a441b8d6d7f609faf381b795d4ba606b2
Christian Paul Flores <florespaul161@gmail.com>
2023-09-22 20:55:14 +0800

    Feature branch (changes on: September 22, 2023)  (#2)
    * feat: update
    
    * ref(TeamCardData): remove id
    
    for easier adding of another cards
    
    * ref(Team): use `index` of every array as index
    
    Efficient for fixed data
    
    * ref(CoreTeamAccordionData): add 4 accordion for 4 terms
    
    * ref(TeamCardData): remove "." on "AY"

--- src/data/CoreTeamAccordionData.js
+++ src/data/CoreTeamAccordionData.js
@@ -1,16 +1,30 @@
 export const CoreTeamAccordionData = [
     {
         id: 1,
+        accordionId: "flush-coreTeam2020",
+        target: "flush-collapseOne",
+        title: "Core Team 2020-2021",
+        cards: "",
+    },
+    {
+        id: 2,
         accordionId: "flush-coreTeam2021",
         target: "flush-collapseTwo",
         title: "Core Team 2021-2022",
         cards: "",
     },
     {
-        id: 2,
-        accordionId: "flush-coreTeam2020",
-        target: "flush-collapseOne",
-        title: "Core Team 2020-2021",
+        id: 3,
+        accordionId: "flush-coreTeam2022",
+        target: "flush-collapseThree",
+        title: "Core Team 2022-2023",
+        cards: "",
+    },
+    {
+        id: 4,
+        accordionId: "flush-coreTeam2023",
+        target: "flush-collapseFour",
+        title: "Core Team 2023-2024",
         cards: "",
     },
 ]
