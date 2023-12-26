6d397beedb677adc1df524c73cea20c5e0510bc4
Christian Paul Flores <florespaul161@gmail.com>
2023-12-26 13:42:38 +0800

    ref(PartnersData): fix file path & add alt prop

--- src/data/PartnersData.js
+++ src/data/PartnersData.js
@@ -2,78 +2,91 @@ export const PartnersData = [
     {
         id: 1,
         link: "#",
-        image: "/partners/gdsc-ustp.png",
-        tooltip: "University of Science and Technology of Southern Philippines"
+        image: "partners/gdsc-ustp.png",
+        tooltip: "University of Science and Technology of Southern Philippines",
+        alt: "University of Science and Technology of Southern Philippines"
     },
     {
         id: 2,
         link: "",
-        image: "/partners/gdsc-xavier.png",
-        tooltip: "Xavier University Ateneo de Cagayan"
+        image: "partners/gdsc-xavier.png",
+        tooltip: "Xavier University Ateneo de Cagayan",
+        alt: "Xavier University Ateno de Cagayan"
     },
     {
         id: 3,
         link: "",
-        image: "/partners/gdsc-blue-eagle.png",
-        tooltip: "Ateneo de Zamboanga University"
+        image: "partners/gdsc-blue-eagle.png",
+        tooltip: "Ateneo de Zamboanga University",
+        alt: "Ateneo de Zamboanga University"
     },
     {
         id: 4,
         link: "",
-        image: "/partners/gdsc-cec.png",
-        tooltip: "Cristal e-College"
+        image: "partners/gdsc-cec.png",
+        tooltip: "Cristal e-College",
+        alt: "Cristal e-College"
     }, {
         id: 5,
         link: "",
-        image: "/partners/gdsc-feu-tech.png",
-        tooltip: "FEU Institute of Technology"
+        image: "partners/gdsc-feu-tech.png",
+        tooltip: "FEU Institute of Technology",
+        alt: "FEU Institute of Technology"
     },
     {
         id: 6,
         link: "",
-        image: "/partners/gdsc-feu.png",
-        tooltip: "FEU Alabang"
+        image: "partners/gdsc-feu.png",
+        tooltip: "FEU Alabang",
+        alt: "FEU Alabang"
     },
     {
         id: 7,
         link: "",
-        image: "/partners/gdsc-upv.png",
-        tooltip: "University of The Philippines Visayas"
+        image: "partners/gdsc-upv.png",
+        tooltip: "University of The Philippines Visayas",
+        alt: "University of The Philippines Visayas"
     },
     {
         id: 8,
         link: "",
-        image: "/partners/gdsc-up-diliman.png",
-        tooltip: "University of the Philippines Diliman"
+        image: "partners/gdsc-up-diliman.png",
+        tooltip: "University of the Philippines Diliman",
+        alt: "University of the Philippines Diliman"
     },
     {
         id: 9,
         link: "",
-        image: "/partners/gdsc-pubstb.png",
-        tooltip: "Polytechnic University of the Philippines - Sto. Tomas Branch"
+        image: "partners/gdsc-pubstb.png",
+        tooltip: "Polytechnic University of the Philippines - Sto. Tomas Branch",
+        alt: "Polytechnic University of the Philippines - Sto."
     },
     {
         id: 10,
         link: "",
-        image: "/partners/gdsc-batstate-malvar.png",
-        tooltip: "Batangas State University Jose P. Laurel Polytechnic College, Malva"
+        image: "partners/gdsc-batstate-malvar.png",
+        tooltip: "Batangas State University Jose P. Laurel Polytechnic College, Malva",
+        alt: "Batangas State University Jose P. Laurel"
     },
     {
         id: 11,
         link: "",
-        image: "/partners/gdsc-fsuu.png",
-        tooltip: "Father Saturnino Urios University"
+        image: "partners/gdsc-fsuu.png",
+        tooltip: "Father Saturnino Urios University",
+        alt: "Father Saturnino Urios University"
     },
     {
         id: 12,
         link: "",
-        image: "/partners/gdsc-uic.png",
-        tooltip: "University of the Immaculate Conception"
+        image: "partners/gdsc-uic.png",
+        tooltip: "University of the Immaculate Conception",
+        alt: "University of the Immaculate Conception"
     },
     {
         id: 13,
         link: "",
-        image: "/partners/gdsc-hau.png",
-        tooltip: "Holy Angel University"
+        image: "partners/gdsc-hau.png",
+        tooltip: "Holy Angel University",
+        alt: "Holy Angel University"
     }
 ]
